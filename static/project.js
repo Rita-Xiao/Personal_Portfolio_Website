9895fdf2d7c5997bdc44d8bd74b79aa935eb5426
Rita Xiao <ritax@andrew.cmu.edu'git config --global user.email ritax@andrew.cmu.educurl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh |>
2025-10-21 15:53:16 -0400

    just added some more details for the information, some parts are rather hard to add text, as most of them are more application, links, images, etc.

--- static/project.js
+++ static/project.js
@@ -5,3 +5,9 @@ function clickedHere(id, url) {
     element.style.cursor = "pointer";
     window.location.href = url;
 }
+
+// Functionality Considerations, likely will not finish before checkin
+// Filter/Sort Projects (just a thought, but could implement)
+// Form Validation - Contact Page 
+// Enable Animation For the Appearance of My Quote or Name (typing effect or fade in effect)
+// Need to think a bit more on other interactions
